b856d82aa9f964f26f0c2d970ab5d14188e5faf7
ionut2497 <ionut.alazaroae@gmail.com>
2017-11-13 00:04:12 +0100

    Commit test

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,29 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'cartalyst/sentinel' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Cartalyst\\Sentinel\\Laravel\\SentinelServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Activation' => 'Cartalyst\\Sentinel\\Laravel\\Facades\\Activation',
+      'Reminder' => 'Cartalyst\\Sentinel\\Laravel\\Facades\\Reminder',
+      'Sentinel' => 'Cartalyst\\Sentinel\\Laravel\\Facades\\Sentinel',
+    ),
+  ),
+);
