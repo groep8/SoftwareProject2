584cbe0c692000abaae07d80b50817de4e333011
ionut2497 <ionut.alazaroae@gmail.com>
2017-12-16 01:46:03 +0100

    Enquetes maken 50%

--- resources/views/hr/index.blade.php
+++ resources/views/hr/index.blade.php
@@ -15,7 +15,15 @@ Laravel
         </div>
         <div class="container">
             <div class="row">
-                
+            <ul class="collection with-header">
+    <li class="collection-header">
+        <h2 class="flow-text">Recent Surveys
+            <span style="float:right;">
+            </span>
+        </h2>
+    </li>
+    
+</ul> 
                 
             </div>
         </div>
--- routes/web_hr.php
+++ routes/web_hr.php
@@ -1,6 +1,6 @@
 <?php 
 Route::group(array('prefix' => 'hr' , 'middleware' => 'hr','as'=>'hr.'), function(){
     Route::get('/',['as' => 'index','uses' =>'MainController@home']);
-    
+    Route::get('survey',['as' => 'survey' , 'uses' => 'SurveyController@home']);
 });
 
